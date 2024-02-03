0aac6b8b7d0fbfef77cbc98fe89ee7da180e66b5
manjeet <manjeetchoudhary97832@gmail.com>
2024-02-03 17:45:57 +0530

    changes

--- index1.js
+++ index1.js
@@ -1,1 +1,25 @@
-const arr=[20,21,20,23,23,25,1,6,20,24,23]
+let l1=[1,5,4]
+//const aa=l1.reverse()
+let l2=[7,5,6]
+
+//const bb=l2.reverse()
+
+var result = parseInt(l1.join(''));
+var result2=  parseInt(l2.join(''));
+//console.log(result,result2)
+const eee=result+result2
+
+var myArray = eee.toString().split('').map(Number);
+//console.log(myArray);
+
+let ae=[]
+async function ii(){
+    for(let i=(l1.length)-1;i=0;i--){
+        await  ae[l1[i]]
+        console.log(l1[i])
+    }
+}
+
+setTimeout(() => {
+    console.log(ae)
+}, 1000);
